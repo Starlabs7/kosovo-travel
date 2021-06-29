c92dce09ea4c41df1f8e8427f3ab8cdfa9e769d4
harisaaaa <bejiqidaris@gmail.com>
2021-06-29 11:27:02 +0200

    adding footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import Header from './components/header/Header';
+import Footer from './components/footer/Footer'
+
 
 function App() {
   return (
     <div className="App">
        <Header />
+       <Footer />
+    
     </div>
   );
 }
