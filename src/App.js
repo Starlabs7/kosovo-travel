41a1462d451bbcfabce7dd83d2dbc7b8252c0cc3
harisaaaa <bejiqidaris@gmail.com>
2021-07-02 10:59:46 +0200

    working o weather

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import Header from './components/header/Header';
 import MainSection from './components/MainSection/MainSection';
 import Footer from './components/footer/Footer'
 
+import { fetchWeather } from './components/api/fetchWeather';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+const App = () => {
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({});
+    
+    const search = async (e) => {
+        if(e.key === 'Enter') {
+            const data = await fetchWeather(query);
+
+            setWeather(data);
+            setQuery('');
+        }
+    }
+
+    return (
+      <div className="App">
        <Header />
        <MainSection />
-       <Footer />
-    
+      
+       
+      <div className="main-container">
+            <input type="text"className="search"placeholder="Search..."value={query}onChange={(e) => setQuery(e.target.value)}onKeyPress={search}/>
+            {weather.main && (
+                <div className="city">
+                    
+                    
+                </div>
+            )}
+        </div>
+        <Footer />
     </div>
-  );
+      
+    );
 }
-
 export default App;
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -13,11 +13,34 @@ export default function footer() {
                     <div className="logo_contanier">
                     <img src={logo2} />
                     </div>
-                    <div className="footer-third">
-                        <h1>Need Help?</h1>
-                        <a href="#"> Terms and Conditions</a>
-                        <a href="#"> Privacy Policy</a>
-                        <a href="#"> Contact Us</a>
+                    <div className="row">
+                      {/* Column1 */}
+                      <div className="footer-third">
+                      <h4>Services</h4>
+                      <ul className="list-unstyled">
+                          <li>Tickets</li>
+                          <li>Places to go</li>
+                          <li>Plan your trip</li>
+                      </ul>
+                      </div>
+                      {/* Column2 */}
+                      <div className="footer-third">
+                      <h4> Need Help?</h4>
+                      <ul className="list-unstyled">
+                          <li>Terms and Conditions</li>
+                          <li>Privacy Policy</li>
+                          <li>Contact Us</li>
+                      </ul>
+                      </div>
+                      {/* Column3 */}
+                      <div className="footer-third">
+                      <h4> About us</h4>
+                      <ul className="list-unstyled">
+                         
+                          
+                      </ul>
+                      </div>
+
                     </div>
                    
                     
